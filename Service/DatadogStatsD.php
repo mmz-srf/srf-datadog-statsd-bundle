317e1f902de5853d9b59168dd9ff58f5ce26da43
Pascal von Bueren <pascal.von.bueren@ymc.ch>
2015-09-21 15:34:23 +0200

    generalizing method signature

--- Service/DatadogStatsD.php
+++ Service/DatadogStatsD.php
@@ -11,8 +11,9 @@ class DatadogStatsD
      * @param string $stat The metric to in log timing info for.
      * @param float $time The ellapsed time (ms) to log
      * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @param array $tags
      **/
-    public function timing($key, $value, $sampleRate, $tags)
+    public function timing($key, $value, $sampleRate = null, $tags = null)
     {
         \Datadogstatsd::timing($key, $value, $sampleRate, $tags);
     }
