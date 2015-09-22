6bf8eac4c858940620027031612f3925f33a264e
Pascal von Bueren <pascal.von.bueren@ymc.ch>
2015-09-22 09:34:54 +0200

    Renamed Bundle

---
+++ SRFDatadogStatsDBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SRF\Bundles\SRFDatadogStatsDBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SRFDatadogStatsDBundle extends Bundle
+{
+     protected $name = 'SRFDatadogStatsDBundle';
+}
